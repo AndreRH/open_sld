6d5d6128de46bc97756bc55dff89c524565d44b3
André Hentschel <nerv@dawncrow.de>
2014-08-05 20:14:43 +0200

    Make use of the example sof file

--- DE0_control.py
+++ DE0_control.py
@@ -2,6 +2,7 @@ from ftdi import list_devices, setvidpid
 from sld_controller import SLD_Controller
 from bitstring import BitArray
 import sys
+import subprocess
 from time import sleep
 
 
@@ -13,24 +14,38 @@ def main():
     if not devs:
         return
 
+    print 'Killing broken jtagd ...'
+    command_line = 'killall jtagd'
+    r = subprocess.call(command_line, shell=True)
+    print 'return code:', r
+
+    print 'Programming ...'
+    command_line = 'quartus_pgm -c \'USB-Blaster\' -m JTAG -o \'p;InitialTest.sof\''
+    r = subprocess.call(command_line, shell=True)
+    print 'return code:', r
+
     sld = SLD_Controller('USB-Blaster', 4, 1)
 
     sld.TAP_Reset()
 
+    d = 127
+    while True:
 
-    sld.VIR_Write(1, BitArray('0b10001'))
+        sld.VIR_Write(1, BitArray('0b10001'))
+        read_back = sld.VDR_Write_Read(BitArray(uint=d, length=7))
+        sld.VIR_Write(1, BitArray('0b10000'))
 
-    sld.VDR_Write(BitArray('0b0000001'))
-    sleep(.3)
-    sld.VDR_Write(BitArray('0b0000010'))
-    sleep(.3)
-    sld.VDR_Write(BitArray('0b0000100'))
-    sleep(.3)
-    sld.VDR_Write(BitArray('0b0001000'))
-    sleep(.3)
+        print read_back.bin
 
-    sld.VIR_Write(1, BitArray('0b10000'))
+        if d == 0:
+            d = 127
+            break
+        else:
+            d -= 1
 
+        sleep(0.1)
+
+    sld.TAP_Reset()
     sld.close()
 
 if __name__ == '__main__':
